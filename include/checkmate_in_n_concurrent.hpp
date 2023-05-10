f01a9329e789a59627687359e30bfd6ab7c9b2be
Jouny Zedan <jounyzedan@dyn208179.cc.lehigh.edu>
2023-05-10 12:10:44 -0400

    shii ain't sliding

--- include/checkmate_in_n_concurrent.hpp
+++ include/checkmate_in_n_concurrent.hpp
@@ -1,8 +1,10 @@
 #include <libchess/position.hpp>
 #include <answer_concurrent.hpp>
+#include <tbb/task_group.h>
 
 using namespace std;
 using namespace libchess;
+using namespace tbb;
 
 class checkmate_in_n_concurrent{
     Position pos;
@@ -12,45 +14,55 @@ class checkmate_in_n_concurrent{
             this->pos = pos;
         }
         
-        // Answer findAnswer(int depth) {
-        //     Answer *answer = new Answer();
-        //     _answerMove(answer, depth);
-        //     return *answer;
-        // }
+        Answer findAnswer(int depth) {
+            Answer *answer = new Answer();
+            task_group g;
+            _answerMove(answer, depth, g);
+            return *answer;
+        }
 
         
-        // bool _answerMove(Answer *answer, int depth) {
-        //     auto moves = this->pos.legal_moves();
-        //     for(const auto &move : moves){
-        //         this->pos.makemove(move);
-        //         bool found = _opponentMove(answer, depth);
-        //         this->pos.undomove();
-        //         if(found)
-        //         {
-        //             answer->setAnswerMove(move);
-        //             return true;
-        //         }
-        //     }
-        //     return false;
-        // }
+        bool _answerMove(Answer *answer, int depth, task_group &g) {
+            auto moves = this->pos.legal_moves();
+            bool found = false;
+            // task_group_context* ctx = current_context();
+            for(const auto &move : moves){
+                g.run([&]() {
+                this->pos.makemove(move);
+                if (!_opponentMove(answer, depth, g)) {
+                    this->pos.undomove();
+                } else {
+                    found = true;
+                    answer->setAnswerMove(move);
+                    g.cancel(); // stop executing other sub-tasks
+                }
+                });
+            }
+            g.wait();
+            return found;
+        }
 
-        // bool _opponentMove(Answer *answer, int depth) {
-        //     auto moves = this->pos.legal_moves();
-        //     if (moves.size() == 0) {
-        //         return this->pos.is_checkmate();
-        //     } else if (depth == 1) {
-        //         return false;
-        //     }
-        //     for(const auto &move : moves){
-        //         this->pos.makemove(move);
-        //         Answer *next = new Answer();
-        //         bool found = _answerMove(next, depth-1);
-        //         this->pos.undomove();
-        //         if (!found) {
-        //             return false;
-        //         }
-        //         answer->putAnswer(move, *next);
-        //     }
-        //     return true;
-        // }
+        bool _opponentMove(Answer *answer, int depth, task_group &g) {
+            auto moves = this->pos.legal_moves();
+            if (moves.size() == 0) {
+                return this->pos.is_checkmate();
+            } else if (depth == 1) {
+                return false;
+            }
+            bool found = true;
+            for (const auto &move : moves) {
+                g.run([&]() {
+                    this->pos.makemove(move);
+                    Answer *next = new Answer();
+                    if (!_answerMove(next, depth - 1, g)) {
+                        this->pos.undomove();
+                    } else {
+                        answer->putAnswer(move, *next);
+                    }
+                    found &= found; // combine results from sub-tasks
+                });
+            }
+            g.wait(); // wait for all sub-tasks to complete
+            return found;
+        }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,10 @@ int main() {
     string fen = "r1bq2k1/ppp2r1p/2np1pNQ/2bNpp2/2B1P3/3P4/PPP2PPP/R3K2R w KQ - 0 1";
     auto pos = libchess::Position(fen, true);
     auto checkmate = checkmate_in_n(pos);
+    auto checkmate_concurrent = checkmate_in_n_concurrent(pos);
     auto answer = checkmate.findAnswer(5);
+    cout << "here" << endl;
+    auto answer_quick = checkmate_concurrent.findAnswer(5);
     auto mv = answer.getAnswerMove();
     while(mv)
     {
