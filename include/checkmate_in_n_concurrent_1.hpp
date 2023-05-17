53e7d7db8e7efa59461b9a2f992c5f0736f529e9
Jouny Zedan <jzz223@cupid.cse.lehigh.edu>
2023-05-17 14:27:23 -0400

    here

--- include/checkmate_in_n_concurrent_1.hpp
+++ include/checkmate_in_n_concurrent_1.hpp
@@ -15,47 +15,49 @@ public:
     checkmate_in_n_concurrent_1(Position pos, int threads_size)
         : pos(pos), threads_size(threads_size) {}
 
-
-    void do_work(int starting_index, int ending_index, vector<Position> &ps, vector<bool> &founded, int depth, int thread_index)
-    {
-        for(int i = starting_index; i<ending_index; i++)
-        {
-            if(_answerMove(depth, ps[i]))
-            {
-                founded[i] = true;
-                break;
-            }
-        }
-    }
-
     // Main function to find the answer
     bool findAnswer(int depth) {
         auto moves = this->pos.legal_moves();
         vector<Position> ps(moves.size());
         parallel_for(blocked_range<int>(0,moves.size()), [&](blocked_range<int> r) {
-            for (int i=r.begin(); i<r.end(); ++i)
-            {
+            for (int i=r.begin(); i<r.end(); ++i) {
                 Position t(this->pos.get_fen());
                 t.makemove(moves[i]);
                 ps[i] = t;
             }
         });
-        vector<bool> founded(moves.size());
+        vector<bool> found(ps.size());
         vector<thread> threads(threads_size);
         for(int i = 0;i<threads.size();i++)
         {
-            threads[i] = thread(&checkmate_in_n_concurrent_1::do_work, this, i * (ps.size()/threads_size), (i+1) * (moves.size()/threads_size), ref(ps), ref(founded), depth, i);
+            int ending_index = (i+1) * (ps.size()/threads_size);
+            if(i == threads.size() - 1) {
+                ending_index = ps.size();
+            }
+            threads[i] = thread(&checkmate_in_n_concurrent_1::do_work, this, i * (ps.size()/threads_size), ending_index, ps, ref(found), depth, i);
         }
-        for(int i = 0;i<founded.size();i++)
-        {
-            if(founded[i])
-            {
+        for(int i = 0;i<threads.size();i++) {
+            threads[i].join();
+        }
+        for(int i = 0;i<found.size();i++) {
+            if(found[i]) {
                 return true;
             }
         }
         return false;
     }
 
+    void do_work(int starting_index, int ending_index, vector<Position> ps, vector<bool> &found, int depth, int thread_index)
+    {
+        for(int i = starting_index; i<ending_index; i++)
+        {
+            if(_opponentMove(depth, ps[i]))
+            {
+                found[i] = true;
+                break;
+            }
+        }
+    }
 
     bool _answerMove(int depth, Position p) {
         auto moves = p.legal_moves();
--- src/main.cpp
+++ src/main.cpp
@@ -26,21 +26,23 @@ int main() {
     // 3qr2k/pbpp2pp/1p5N/3Q2b1/2P1P3/P7/1PP2PPP/R4RK1 w - - 0 1
     // 9 moves
     // 6q1/3r1p2/2N1nk1K/3rp3/8/5PP1/8/1Q1N4 w - - 0 1
-    string fen = "3qr2k/pbpp2pp/1p5N/3Q2b1/2P1P3/P7/1PP2PPP/R4RK1 w - - 0 1";
+    string fen = "6q1/3r1p2/2N1nk1K/3rp3/8/5PP1/8/1Q1N4 w - - 0 1";
     timer stopwatch;
     auto pos = libchess::Position(fen, true);
     auto checkmate = checkmate_in_n(pos);
     auto modified = checkmate_in_n_modified(pos);
     auto checkmate_concurrent = checkmate_in_n_concurrent(pos);
-    auto checkmate_concurrent_1 = checkmate_in_n_concurrent_1(pos, 4);
+    unsigned int numThreads = std::thread::hardware_concurrency();
+    auto checkmate_concurrent_1 = checkmate_in_n_concurrent_1(pos, 8);
     stopwatch.elapsed();
-    bool answer = checkmate.findAnswer(2);
+    bool answer = checkmate.findAnswer(6);
     cout << "Checkmate-In-N Sequential Returned " << answer << " with an execution time: " << stopwatch.elapsed() << endl;
     // bool answer_modified = modified.findAnswer(7);
     // cout << "Checkmate-In-N Modified Returned " << answer_modified << " with an execution time: " << stopwatch.elapsed() << endl;
-    bool answer_concurrent = checkmate_concurrent.findAnswer(2);
-    cout << "Checkmate-In-N Concurrent Returned " << answer_concurrent << " with an execution time: " << stopwatch.elapsed() << endl;
-    bool answer_concurrent_1 = checkmate_concurrent_1.findAnswer(2);
+    // bool answer_concurrent = checkmate_concurrent.findAnswer(5);
+    // cout << "Checkmate-In-N Concurrent Returned " << answer_concurrent << " with an execution time: " << stopwatch.elapsed() << endl;
+    printf("Running the concurrent version with %d threads\n", numThreads);
+    bool answer_concurrent_1 = checkmate_concurrent_1.findAnswer(6);
     cout << "Checkmate-In-N Concurrent_1 Returned " << answer_concurrent_1 << " with an execution time: " << stopwatch.elapsed() << endl;
     return 0;
 }
